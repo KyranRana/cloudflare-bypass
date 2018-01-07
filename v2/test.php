48f74179f757b167f546564a67233439d1484a50
Kyran Rana <kyranportfolio@gmail.com>
2018-01-07 13:48:26 +0000

    Code cleanup 6

--- v2/test.php
+++ v2/test.php
@@ -15,8 +15,9 @@ if($curl_handle = curl_init("https://anime47.com"))
 
     curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36');
 
+    echo "Request begin!\n";
     $res = $cloudflare_bypass->curlExec($curl_handle);
-    echo $res;
-
+    echo "Request ended with: " . curl_getinfo($curl_handle)['http_code'] . "\n";
+    
     curl_close($curl_handle);
 }
