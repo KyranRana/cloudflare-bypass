3b5024c9c97f32b0e594ba767b12071f772091aa
Kyran Rana <kyranportfolio@gmail.com>
2018-03-18 23:29:23 +0000

    Added more test coverage for cURL and stream. Cache specific assertions included.

--- v2/tests/CurlTest.php
+++ v2/tests/CurlTest.php
@@ -6,13 +6,18 @@ use CloudflareBypass\RequestMethod\CFCurl;
 
 class CurlTest extends TestCase
 {
- 
     /**
-     * Url to test
+     * Urls to test
      *
      * @var string
      */
-    protected $url = "http://thebot.net/";
+    protected $urls = [
+        "http://gnula.nu/",
+        "https://animeflv.net/",
+        "https://thebot.net/",
+        "https://coinkite.com/",
+        "http://dll.anime47.com/imgur/"
+    ];
     
     /**
      * Test 503 (without bypass)
@@ -21,12 +26,15 @@ class CurlTest extends TestCase
      */
     public function test503()
     {
-        $ch = curl_init($this->url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_exec($ch);
- 
-        $this->assertEquals(503, curl_getinfo($ch, CURLINFO_HTTP_CODE));
+        foreach ($this->urls as $url) {
+            // Make sure each site is protected by CF.
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_exec($ch);
+
+            $this->assertEquals(503, curl_getinfo($ch, CURLINFO_HTTP_CODE));
+        }
     }
 
     /**
@@ -36,19 +44,36 @@ class CurlTest extends TestCase
      */
     public function test200()
     {
+        // Initialize CFCurl wrapper.
         $curl_cf_wrapper = new CFCurl(array(
-            'cache'         => true,   // Caching now enabled by default; stores clearance tokens in Cache folder
-            'max_attempts'  => 5       // Max attempts to try and get CF clearance
+            'cache'         => true,
+            'max_attempts'  => 5
         ));
-        
-        $ch = curl_init($this->url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
- 
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36');
-        $response = $curl_cf_wrapper->exec($ch); // Done! NOTE: HEAD requests not supported!
- 
-        $this->assertEquals(200, curl_getinfo($ch, CURLINFO_HTTP_CODE));
-        curl_close($ch);
+
+        foreach ($this->urls as $url) {
+            // Bypass each site using CFCurl wrapper.
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36');
+
+            $response = $curl_cf_wrapper->exec($ch);
+
+            // Parse url into components.
+            $url_components = parse_url($url);
+
+            // Get cache file (path included).
+            $cache_file = __DIR__ . '/../src/CloudflareBypass/Cache/' . md5($url_components['host']);
+
+            $this->assertEquals(200, curl_getinfo($ch, CURLINFO_HTTP_CODE));
+            $this->assertEquals(true, file_exists($cache_file));
+            $this->assertEquals(true, isset(json_decode(file_get_contents($cache_file))->cf_clearance));
+
+            // Remove the file from cache.
+            unlink($cache_file);
+
+            curl_close($ch);
+        }
     }
 }
--- v2/tests/GuzzleHttpTest.php
+++ v2/tests/GuzzleHttpTest.php
@@ -10,12 +10,18 @@ class GuzzleHttpTest extends TestCase
 {
 
     /**
-     * Url to test
+     * Urls to test
      *
      * @var string
      */
-    protected $url = "https://coinkite.com";
-    
+    protected $urls = [
+        "http://gnula.nu/",
+        "https://animeflv.net/",
+        "https://thebot.net/",
+        "https://coinkite.com/",
+        "http://dll.anime47.com/imgur/"
+    ];
+
     /**
      * Test 503 (without bypass)
      *
@@ -23,24 +29,22 @@ class GuzzleHttpTest extends TestCase
      */
     public function test503()
     {
-        $stream_cf_wrapper = new CFStream(array(
-            'cache'         => true,  // Caching now enabled by default; stores clearance tokens in Cache folder
-            'max_attempts'  => 5      // Max attempts to try and get CF clearance
-        ));
-
         $agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
 
         $client = new Client();
 
-        $response = $client->request('GET', $this->url, [
-            'headers' => [
-                'User-Agent' => "$agent",
-            ],
-            'http_errors' => false,
-            // 'debug' => true
-        ]);
+        foreach ($this->urls as $url) {
+            // Make sure each site is protected by CF.
+            $response = $client->request('GET', $url, [
+                'headers' => [
+                    'User-Agent' => "$agent",
+                ],
+                'http_errors' => false
+                // 'debug' => true
+            ]);
 
-        $this->assertEquals(503, $response->getStatusCode());
+            $this->assertEquals(503, $response->getStatusCode());
+        }
     }
 
     /**
@@ -50,33 +54,48 @@ class GuzzleHttpTest extends TestCase
      */
     public function test200()
     {
+        // Initialize CFStream.
         $stream_cf_wrapper = new CFStream(array(
-            'cache'         => true,  // Caching now enabled by default; stores clearance tokens in Cache folder
-            'max_attempts'  => 5      // Max attempts to try and get CF clearance
+            'cache'         => true,
+            'max_attempts'  => 5
         ));
 
         $agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
 
         $opts = array(
             'http' => array(
-                'method' => "GET",
-                'header' => "User-Agent:$agent"
+                'method'    => "GET",
+                'header'    => "User-Agent:$agent"
             )
         );
 
-        $stream = $stream_cf_wrapper->create($this->url, $opts);
         $client = new Client();
 
-        $cookieJar = CookieJar::fromArray($stream->getCookiesOriginal(), parse_url($this->url)['host']);
+        foreach ($this->urls as $url) {
+            // Parse url into components.
+            $components = parse_url($url);
 
-        $response = $client->request('GET', $this->url, [
-            'headers' => [
-                'User-Agent' => "$agent",
-            ],
-            'cookies' => $cookieJar,
-            // 'debug' => true
-        ]);
+            // Bypass each site using CFStream wrapper.
+            $stream     = $stream_cf_wrapper->create($url, $opts);
+            $cookie_jar = CookieJar::fromArray($stream->getCookiesOriginal(), $components['host']);
 
-        $this->assertEquals(200, $response->getStatusCode());
+            $response = $client->request('GET', $url, [
+                'headers' => [
+                    'User-Agent' => "$agent",
+                ],
+                'cookies' => $cookie_jar,
+                // 'debug' => true
+            ]);
+
+            // Get cache file (path included).
+            $cache_file = __DIR__ . '/../src/CloudflareBypass/Cache/' . md5($components['host']);
+
+            $this->assertEquals(200, $response->getStatusCode());
+            $this->assertEquals(true, file_exists($cache_file));
+            $this->assertEquals(true, isset(json_decode(file_get_contents($cache_file))->cf_clearance));
+
+            // Remove the file from cache.
+            unlink($cache_file);
+        }
     }
 }
