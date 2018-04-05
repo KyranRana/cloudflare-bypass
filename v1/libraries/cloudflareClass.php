38557b4b2d8753ebb01159b1222e6efe39c468e7
dreamdeveloper <10743142+dreamdeveloper@users.noreply.github.com>
2018-04-05 18:24:08 +0300

    Update cloudflareClass.php
    Fixed issue for the error "Call to undefined function toFixed()"

--- v1/libraries/cloudflareClass.php
+++ v1/libraries/cloudflareClass.php
@@ -262,6 +262,8 @@ class cloudflare {
 		$script  = preg_replace("/'[^']+'/", "", $script);
 		// Fix
 		$script  = str_replace('f.action+=location.hash;', '', $script);
+		// Fixed issue for the error "Call to undefined function toFixed()"
+		$script  = str_replace('.toFixed(10)' , '', $script);
 		// evaluate PHP script
 		eval($script);
 		// if cloudflare answer has been found, store it 
